17efde2d2b1d2a6f3d05318a616ad0ddd5954a39
JorgeRinconPerez <85314943+JorgeRinconPerez@users.noreply.github.com>
2022-03-20 14:30:31 +0100

    Encoding App (trial 2)

--- app.py
+++ app.py
@@ -3,14 +3,29 @@ import pandas as pd
 import numpy as np
 
 st.write("""
-# ¡Hola, Monica! 👋
+# ¡Hola, Mónica! 👋
 Esta app te ayudará con problemas de encoding.
 """)
 
 uploaded_file = st.file_uploader("", type=['csv'])
 if uploaded_file is not None:
   #read csv
-  df1=pd.read_csv(uploaded_file)
-else:
-  st.warning("Necesitas cargar el archivo csv")
+  df=pd.read_csv(uploaded_file, sep=";")
+  st.write("Estas son las 10 primeras filas de tu archivo corregido")
+  st.write(df.head(10))
+  st.write("\n")
 
+  @st.cache
+  def convert_df(df):
+    return df.to_csv(encoding="mbcs").encode('utf-8')
+  csv = convert_df(df)
+  st.write("\n")
+  st.download_button(
+    "Pulsa para descargar",
+    csv,
+    "Audited data.csv",
+    "text/csv",
+    key='download-csv')
+
+else:
+  st.warning("Para empezar necesitas cargar un archivo .csv")
