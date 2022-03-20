d0f6d378b06d764bd6b2a84e04720484af59ff60
JorgeRinconPerez <85314943+JorgeRinconPerez@users.noreply.github.com>
2022-03-20 15:17:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if uploaded_file is not None:
 
   @st.cache
   def convert_df(df):
-    return df.to_csv(encoding="mbcs").encode('mbcs').decode('uft-8)
+    return df.to_csv(encoding="mbcs").encode('mbcs').decode('uft-8')
   csv = convert_df(df)
   st.write("\n")
   st.download_button(
